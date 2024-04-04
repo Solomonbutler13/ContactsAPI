801d3c23d9c0fe05a25935a3579aa7f30ba11683
Solomon <solomonbutler13sb@gmail.com>
2024-04-03 21:28:41 -0400

    ContactsAPI

---
+++ index.js
@@ -0,0 +1,23 @@
+const db = require('./queries.js');
+const express = require('express'); 
+const cors = require('cors');
+const app = express(); 
+const port = 3030;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request, response) => { 
+    response.json({ info: 'Node.js, Express and Postgres API'}) 
+})
+
+app.get('/contacts', db.getContacts);
+app.get('/contact', db.getContact);
+app.post('/contacts', db.addContact);
+app.delete("/contact/:id", db.deleteContact);
+app.put('/contacts', db.updateContact);
+
+
+app.listen(port, () => { 
+    console.log(`App running on port ${port}.`) 
+})
